fe28322e9a5d3d55294d6952ff38efa113ea7e0a
wuhaitao <wuhaitao1@xiaomi.com>
2020-03-17 14:28:16 +0800

    style: replace letui as elementui in user-mnage.vue

--- client/src/pages/server/user-manage.vue
+++ client/src/pages/server/user-manage.vue
@@ -1,31 +1,45 @@
 <template>
   <div>
-    <let-form inline itemWidth="600px" @submit.native.prevent="save">
-      <let-form-item :label="$t('user.op')">
-        <let-input type="textarea" v-model="operator" :disabled="!hasAuth"></let-input>
-      </let-form-item>
-      <let-form-item :label="$t('user.dev')">
-        <let-input type="textarea" v-model="developer" :disabled="!hasAuth"></let-input>
-      </let-form-item>
-      <let-form-item>
-      <let-button type="submit" theme="primary" v-if="hasAuth">{{$t('common.submit')}}</let-button>
-      </let-form-item>
-    </let-form>
+    <div style="width:600px">
+      <el-form @submit.native.prevent="save">
+        <el-form-item :label="$t('user.op')">
+          <el-input
+            type="textarea"
+            v-model="operator"
+            :disabled="!hasAuth"
+          ></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('user.dev')">
+          <el-input
+            type="textarea"
+            v-model="developer"
+            :disabled="!hasAuth"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            @click="save"
+            type="primary"
+            theme="primary"
+            v-if="hasAuth"
+            >{{ $t("common.submit") }}</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'ServerUserManage',
+  name: "ServerUserManage",
 
   data() {
     return {
-        serverData: Object.assign({}, this.$parent.getServerData()),
-        developer: '',
-        operator: '',
-        hasAuth: false,
+      serverData: Object.assign({}, this.$parent.getServerData()),
+      developer: "",
+      operator: "",
+      hasAuth: false
     };
   },
 
@@ -35,33 +49,60 @@ export default {
   },
 
   methods: {
-    checkHasAuth(){
-        this.$ajax.getJSON('/server/api/has_auth', {application: this.serverData.application, server_name: this.serverData.server_name, role: 'developer'}).then((data)=>{
-          console.log('checkHasAuth', data);
-          this.hasAuth = data.has_auth || false;
-        }).catch((err)=>{
-          this.$tip.error(`${this.$t('common.error')}: ${err.message || err.err_msg}`);
+    checkHasAuth() {
+      this.$ajax
+        .getJSON("/server/api/has_auth", {
+          application: this.serverData.application,
+          server_name: this.serverData.server_name,
+          role: "developer"
         })
+        .then(data => {
+          console.log("checkHasAuth", data);
+          this.hasAuth = data.has_auth || false;
+        })
+        .catch(err => {
+          this.$tip.error(
+            `${this.$t("common.error")}: ${err.message || err.err_msg}`
+          );
+        });
     },
-    getAuthList(){
-      this.$ajax.getJSON('/server/api/get_auth_list', {application: this.serverData.application, server_name: this.serverData.server_name}).then((data)=>{
-        this.operator = (data.operator||[]).join(';')
-        this.developer = (data.developer||[]).join(';')
-      }).catch((err)=>{
-          this.$tip.error(`${this.$t('common.error')}: ${err.message || err.err_msg}`);
-      })
+    getAuthList() {
+      this.$ajax
+        .getJSON("/server/api/get_auth_list", {
+          application: this.serverData.application,
+          server_name: this.serverData.server_name
+        })
+        .then(data => {
+          this.operator = (data.operator || []).join(";");
+          this.developer = (data.developer || []).join(";");
+        })
+        .catch(err => {
+          this.$tip.error(
+            `${this.$t("common.error")}: ${err.message || err.err_msg}`
+          );
+        });
     },
-    save(){
+    save() {
       const loading = this.$Loading.show();
-      this.$ajax.postJSON('/server/api/update_auth', {application: this.serverData.application, server_name: this.serverData.server_name, operator: this.operator, developer: this.developer}).then((data)=>{
-        loading.hide();
-        this.$tip.success(`${this.$t('common.success')}`);
-      }).catch((err)=>{
-        loading.hide();
-        this.$tip.error(`${this.$t('common.error')}: ${err.message || err.err_msg}`);
-      })
+      this.$ajax
+        .postJSON("/server/api/update_auth", {
+          application: this.serverData.application,
+          server_name: this.serverData.server_name,
+          operator: this.operator,
+          developer: this.developer
+        })
+        .then(data => {
+          loading.hide();
+          this.$tip.success(`${this.$t("common.success")}`);
+        })
+        .catch(err => {
+          loading.hide();
+          this.$tip.error(
+            `${this.$t("common.error")}: ${err.message || err.err_msg}`
+          );
+        });
     }
-  },
+  }
 };
 </script>
 

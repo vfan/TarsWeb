1ef3416c586f5bfbf649a0c1ebd0c715d910d736
wuhaitao <wuhaitao1@xiaomi.com>
2020-04-14 12:10:13 +0800

    style: check framework page(letui->elementui)

--- client/src/pages/operation/check.vue
+++ client/src/pages/operation/check.vue
@@ -1,30 +1,28 @@
 <template>
-  <div class="page_operation_templates"  style="text-align: center;" >
-    <let-button size="small" theme="primary" @click="checkFramework">{{$t('nodes.btn.check')}}</let-button>
-    <let-button theme="info" style="float:right" @click="openShowProblem">有问题怎么办?</let-button>
-    <br><br>
-    <let-table ref="checkLoading" :data="servers" stripe :row-class-name="tableRowClassName" :empty-msg="$t('common.nodata')">
-      <let-table-column :title="$t('checkTable.table.th.server_name')" prop="serverName"></let-table-column>
-      <let-table-column :title="$t('checkTable.table.th.node_name')" prop="nodeName"></let-table-column>
-      <let-table-column :title="$t('checkTable.table.th.obj_name')" prop="objName"></let-table-column>
-      <let-table-column :title="$t('checkTable.table.th.status')" prop="status"></let-table-column>
-    </let-table>
-    <let-modal :title="$t('checkTable.problemDialog.title')" v-model="showProblem"  width="600px">
+  <div class="page_operation_templates">
+    <el-button size="small" type="primary" @click="checkFramework">{{ $t('nodes.btn.check') }}</el-button>
+    <el-button size="small" type="info" style="float: right;" @click="openShowProblem">有问题怎么办?</el-button>
+    <br /><br />
+    <el-table ref="checkLoading" :data="servers" stripe :row-class-name="tableRowClassName" :empty-msg="$t('common.nodata')">
+      <el-table-column width="200" :label="$t('checkTable.table.th.server_name')" prop="serverName"></el-table-column>
+      <el-table-column :label="$t('checkTable.table.th.node_name')" prop="nodeName"></el-table-column>
+      <el-table-column :label="$t('checkTable.table.th.obj_name')" prop="objName"></el-table-column>
+      <el-table-column width="200" :label="$t('checkTable.table.th.status')" prop="status"></el-table-column>
+    </el-table>
+    <el-dialog :visible.sync="showProblem" :title="$t('checkTable.problemDialog.title')" v-model="showProblem" width="600px">
       <div>
-        <br>
-        <p>{{$t('checkTable.problemDialog.info')}}</p>
-        <br>
-        <p>{{$t('checkTable.problemDialog.restartFramework')}}</p>
-        <p>{{$t('checkTable.problemDialog.stopFramework')}}</p>
-        <p>{{$t('checkTable.problemDialog.restartServer')}}</p>
+        <br />
+        <p>{{ $t('checkTable.problemDialog.info') }}</p>
+        <br />
+        <p>{{ $t('checkTable.problemDialog.restartFramework') }}</p>
+        <p>{{ $t('checkTable.problemDialog.stopFramework') }}</p>
+        <p>{{ $t('checkTable.problemDialog.restartServer') }}</p>
       </div>
-    </let-modal>
+    </el-dialog>
   </div>
-
 </template>
 
 <script>
-
 export default {
   name: 'OperationCheck',
 
@@ -32,107 +30,110 @@ export default {
     return {
       showProblem: false,
       servers: []
-    };
+    }
   },
 
-  mounted() {
-  },
+  mounted() {},
 
   methods: {
     openShowProblem() {
-      this.showProblem = true;
+      this.showProblem = true
     },
-    tableRowClassName({row, rowIndex}) {
+    tableRowClassName({ row, rowIndex }) {
       if (row.check === 1) {
-        return '';
+        return ''
       } else if (row.check === 2) {
-        return "checking-row";
+        return 'checking-row'
       } else if (row.check === 0) {
-        return 'success-row';
+        return 'success-row'
       } else if (row.check === -1) {
-        return 'warning-row';
+        return 'warning-row'
       }
-      return '';
+      return ''
     },
     checkFramework() {
-      const loading = this.$refs.checkLoading.$loading.show();
-      this.$ajax.getJSON('/server/api/get_framework_list').then((data) => {
+      // const loading = this.$refs.checkLoading.$loading.show()
+      this.$ajax
+        .getJSON('/server/api/get_framework_list')
+        .then(data => {
+          // loading.hide()
+          for (var i = 0; i < data.servers.length; i++) {
+            data.servers[i].check = 1
+            data.servers[i].status = 'waiting'
+          }
 
-        loading.hide();
-        for(var i = 0; i < data.servers.length; i++)
-        {
-          data.servers[i].check  = 1;
-          data.servers[i].status = "waiting";
-        }
+          this.servers = data.servers
 
-        this.servers = data.servers;
-
-        var that = this;
-        for(var i = 0; i < that.servers.length; i++)
-        {
-          (function(i) {
-            setTimeout(function(){
-              that.checkServer(i)
-            }, i*100);
-          })(i);
-        }
-      }).catch((err) => {
-        loading.hide();
-
-        this.$Notice({
-          title: this.$t('checkTable.adminRegistryFailed'),
-          message: this.$t('checkTable.restartAdminRegistry'),
-          type: 'error',
-          duration: 0
+          var that = this
+          for (var i = 0; i < that.servers.length; i++) {
+            ;(function (i) {
+              setTimeout(function () {
+                that.checkServer(i)
+              }, i * 100)
+            })(i)
+          }
         })
-      });
+        .catch(err => {
+          // loading.hide()
+
+          console.error(err)
+          this.$notify({
+            title: this.$t('checkTable.adminRegistryFailed'),
+            message: this.$t('checkTable.restartAdminRegistry'),
+            type: 'error',
+            duration: 0
+          })
+        })
     },
     checkServer(index) {
-      var server = this.servers[index];
+      var server = this.servers[index]
 
-      server.check  = 2;
-      server.status = "checking"; 
+      server.check = 2
+      server.status = 'checking'
 
-      this.$ajax.postJSON('/server/api/check_framework_server', {server:server}).then((data) => {
-        server.check  = 0;
-        server.status = "succ";
-
-      }).catch((err) => {
-        server.check  = -1;
-        server.status = this.$t('checkTable.failed');
-      });
+      this.$ajax
+        .postJSON('/server/api/check_framework_server', { server: server })
+        .then(data => {
+          server.check = 0
+          server.status = 'succ'
+        })
+        .catch(err => {
+          console.error('checkserver', err)
+          server.check = -1
+          server.status = this.$t('checkTable.failed')
+        })
     }
-  },
-};
+  }
+}
 </script>
 
 <style scoped>
 .page_operation_templates {
   pre {
-    color: #909FA3;
+    color: #909fa3;
     margin-top: 32px;
   }
 }
 
-.let-table tr.checking-row td {
+.el-table tr.checking-row td {
   background: burlywood !important;
-  color: #FFF;
+  color: #fff;
 }
-.let-table tr.warning-row td {
-  background: #F56C77 !important;
-  color: #FFF;
+.el-table tr.warning-row td {
+  background: #f56c77 !important;
+  color: #fff;
 }
-.let-table tr.success-row td {
-  background: #6ACCAB !important;
-  color: #FFF;
+.el-table tr.success-row td {
+  background: #6accab !important;
+  color: #fff;
 }
-.let-table tr.checking-row td .let-table__operation {
-  color: #FFF;
+.el-table tr.checking-row td .el-table__operation {
+  color: #fff;
 }
-.let-table tr.warning-row td .let-table__operation {
-  color: #FFF;
+.el-table tr.warning-row td .el-table__operation {
+  color: #fff;
 }
-.let-table tr.success-row td .let-table__operation {
-  color: #FFF;
+.el-table tr.success-row td .el-table__operation {
+  color: #fff;
 }
 </style>
